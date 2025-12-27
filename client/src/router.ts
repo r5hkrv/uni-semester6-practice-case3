d71b2ccf2803a54629fc044dcc5e436d97fbf730
r5hkrv <202286897+r5hkrv@users.noreply.github.com>
2025-12-27 23:17:36 +0400

    (client) add a not found route

--- client/src/router.ts
+++ client/src/router.ts
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "./views/HomeView.vue";
 import SignUpView from "./views/SignUpView.vue";
 import SignInView from "./views/SignInView.vue";
+import NotFoundView from "./views/NotFoundView.vue";
 
 const routes = [
 	{ path: "/", component: HomeView },
 	{ path: "/signup", component: SignUpView },
 	{ path: "/signin", component: SignInView },
+	{ path: "/:pathMatch(.*)", component: NotFoundView },
 ];
 
 export const router = createRouter({
